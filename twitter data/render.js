186489adfa60c9e69db841a748e14062f612b272
kingshukbasak <kingshuk@fusioncharts.com>
2015-08-23 17:18:35 +0530

    Twitter data modified

--- twitter data/render.js
+++ twitter data/render.js
@@ -79,7 +79,14 @@ window.onload=function()
 	    }
 	}
 	function category() {
-		var paper = new Raphael(document.getElementById('maindiv'), 900, 5000);
+		var paper =  Raphael(100,50, 1230, 1050);
+		/*var path_b = paper.path("m 453.82593,412.80619 c -6.3097,2.79897 -13.09189,4.68982 -20.20852,5.54049 7.26413,-4.35454 12.84406,-11.24992 15.47067,-19.46675 -6.79934,4.03295 -14.3293,6.96055 -22.34461,8.53841 -6.41775,-6.83879 -15.56243,-11.111 -25.68298,-11.111 -19.43159,0 -35.18696,15.75365 -35.18696,35.18525 0,2.75781 0.31128,5.44359 0.91155,8.01875 -29.24344,-1.46723 -55.16995,-15.47582 -72.52461,-36.76396 -3.02879,5.19662 -4.76443,11.24048 -4.76443,17.6891 0,12.20777 6.21194,22.97747 15.65332,29.28716 -5.76773,-0.18265 -11.19331,-1.76565 -15.93716,-4.40083 -0.004,0.14663 -0.004,0.29412 -0.004,0.44248 0,17.04767 12.12889,31.26806 28.22555,34.50266 -2.95247,0.80436 -6.06101,1.23398 -9.26989,1.23398 -2.2673,0 -4.47114,-0.22124 -6.62011,-0.63114 4.47801,13.97857 17.47214,24.15143 32.86992,24.43441 -12.04227,9.43796 -27.21366,15.06335 -43.69965,15.06335 -2.84014,0 -5.64082,-0.16722 -8.39349,-0.49223 15.57186,9.98421 34.06703,15.8094 53.93768,15.8094 64.72024,0 100.11301,-53.61524 100.11301,-100.11387 0,-1.52554 -0.0343,-3.04251 -0.10204,-4.55261 6.87394,-4.95995 12.83891,-11.15646 17.55618,-18.21305 z");
+		var group_a = paper.set(); 
+		path_b.attr({fill: '#2aa9e0',parent: 'group_a','stroke-width': '0','stroke-opacity': '1'});
+		//.transform("t-282.32053,-396.30734").data('id', 'path_b'); 
+		group_a.attr({'name': 'group_a'});
+		group_a.transform("t-282.32053,-396.30734"); 
+		var rsrGroups = [group_a]; group_a.push( path_b );	*/
 		/*for (var i = 0; i < 5; i++) {
     	  paper.circle(10 + 15 * i, 10, 10)
          .attr({fill: "#000"})
@@ -89,37 +96,22 @@ window.onload=function()
          });
 		}*/
 		createElements({ element: "div", parentelement: "maindiv", id:"sort",
-		style: { left:"965px",position:"absolute",top:"19px",width:"203px"} });
+		style: { left:"254px",position:"absolute",top:"11px",width:"873px"} });
 		createElements({ element: "label", parentelement: "sort", innerHTML:"SORT BY",
 		style: { border: "1px solid black", padding: "5px 60px 5px 33px" } });
 		createElements({ element: "select", parentelement: "sort", id: "selectcategory",
-			style: { padding: "5px 60px 5px 60px",margin: "10px 0px"},
-			addEventListener: [["click", sortCategory]] });
-		createElements({ element: "option", parentelement: "selectcategory", number:7,innerHTML:["SELECT","View All","Impassioned","Exclamatory","Quotation","Promotion","Hashtags","@tags","Vernaculars"], id: ["select","","impassioned","!!","~","http","#","@","vernaculars"]});
+			style: { padding: "5px 60px 5px 60px",margin: "10px 2px"},
+			addEventListener: [["change", sortCategory]] });
+		createElements({ element: "option", parentelement: "selectcategory", number:7,innerHTML:["SELECT","View All","Impassioned","Exclamatory","Quotation","Promotion","Hashtags","@tags","Vernaculars"],selected:["","selected"], id: ["select","","impassioned","!!","~","http","#","@","vernaculars"]});
 		createElements({ element: "label", parentelement: "sort", innerHTML:"SORT BY",
 		style: { border: "1px solid black", padding: "5px 60px 5px 33px" } });
 		createElements({ element: "select", parentelement: "sort", id: "selectsort",
-			style: { padding: "5px 60px 5px 60px",margin: "10px 0px"},
-			addEventListener: [["click", sortData]] });
-		createElements({ element: "option", parentelement: "selectsort", number:3,innerHTML:["SELECT","RETWEET","FAVOURITES","CREATED AT"], id: ["select1","retweet","favcount","createdat"]});
-		//createElements({ element: "option", parentelement: "selectsort",innerHTML:"FAVOURITES", id: "favcount"});
-		//createElements({ element: "option", parentelement: "selectsort",innerHTML:"CREATED AT", id: "createdat"});
+			style: { padding: "5px 60px 5px 60px",margin: "10px 2px"},
+			addEventListener: [["change", sortData]] });
+		createElements({ element: "option", parentelement: "selectsort", number:3,innerHTML:["SELECT","RETWEET","FAVOURITES","CREATED AT"],selected:["","selected"], id: ["select1","retweet","favcount","createdat"]});
 
 
 		var obj,tweet={},k,xspace,yspace,m,i,objlength,strlength,xsize,ysize;
-
-
-		/*for(k=0;k<length;k++) {
-				if(data[k].text.search('http')!=-1)
-				{
-					tweet={ retweet:data[k].retweet_count,
-	    					favcount:data[k].favorite_count,
-	    					folllowcount:data[k].user.followers_count,
-	    					createdat:data[k].created_at,
-	    					tweet:data[k].text};
-	    			obj[m++]=tweet;
-				}
-		}*/
 		function sortCategory() {
 			paper.clear();
 			var option=document.getElementById("selectcategory"),
@@ -179,14 +171,14 @@ window.onload=function()
 		function sortData() {
 			paper.clear();
 			xspace=0;
-			yspace=0;
-			xsize=5;
-			ysize=5;
+			yspace=80;
+			xsize=10;
+			ysize=10;
 			objlength=obj.length;
 
 			var option=document.getElementById("selectsort"),
 			opacity=1/objlength,
-			sizeincrement=0.5,
+			sizeincrement=40/objlength,
 			anim;
 			option=option.options[option.selectedIndex].id;
 			if(option=='select1')
@@ -194,34 +186,42 @@ window.onload=function()
 			obj.sort(function(a, b){
  				return a[option]-b[option];
 			});
+			
 			for(i=0;i<objlength;i++) {
 				xsize+=sizeincrement;
 				ysize+=sizeincrement;
-				if(xspace+xsize>=900)
-					{
+				if(xspace+xsize>=1000) {
 						yspace+=ysize+3;
 						xspace=0;
-					}
-				var rect=paper.image("twitter.png",-50, -50, xsize, ysize);
-				rect.attr({'title' : obj[i].tweet, "opacity":opacity*i} );		
+				}
+				var tweets=paper.image("twitter.svg",-100, -100, xsize, ysize);
+				tweets.attr({'title' : obj[i].tweet, "opacity":1} );		
 				anim = Raphael.animation({x: xspace, y: yspace}, 100);
-				rect.animate(anim.delay(100*(i+1)));
-				rect.glow({width:0.5,opacity:0.5,color:"blue",offsetx: xspace+50,offsety: yspace+50});
+				tweets.animate(anim.delay(100*(i+1)));
+				tweets.mouseover((function viewTweet(obj,xspace,yspace,tweets) {
+									return function() {
+										var stretch=tweets.getBBox().x2-tweets.getBBox().x;
+										//console.log(tweets.getBBox());
+										//var rect=paper.rect(xspace+stretch,yspace,100,100);
+										var text=paper.set();
+										text.push(
+										paper.text(xspace+stretch+40,yspace,"Retweet: "+obj.retweet),
+										paper.text(xspace+stretch+60,yspace+10,"Favourite Count: "+obj.favcount),
+										paper.text(xspace+stretch+118,yspace+20,"Created At: "+obj.createdat)
+										);
+										tweets.mouseout(function(){
+											text.remove();
+										});
+									}
+								})(obj[i],xspace,yspace,tweets));
 				xspace+=xsize+3;
 
 				
 			}
 		}
+		sortCategory();
+		sortData();
 	}
 	category();
 };
 
-/*window.onload = function() {
-    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
-    var circle = paper.circle(100, 100, 80);
-
-    var anim = Raphael.animation({cx: 10, cy: 20}, 2e3);
-circle.animate(anim); // run the given animation immediately
-//circle2.animate(anim.delay(500)); // run the given animation after 500 ms
-};
-*/
